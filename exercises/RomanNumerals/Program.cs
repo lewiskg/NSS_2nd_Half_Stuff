f60e4394687fb4c568df0a6581ba49b7f1f01d59
lewiskg <kenneth.g.lewis2@gmail.com>
2018-02-12 17:18:14 -0600

    Roman numerals added.

---
+++ exercises/RomanNumerals/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RomanNumerals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Number to Roman Numeral Convertion Program ****");
+            Console.WriteLine("Enter a positive integer number '=' to or '<' 1E6: ");
+            string snum = Console.ReadLine();
+
+            string romanNum = Roman.NumToRoman(snum);
+
+            Console.WriteLine($"{snum} = {romanNum}");
+        }
+    }
+}
